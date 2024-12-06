b6af8f95ad464e16e69f5818c23568947d815635
Rodion Naboka <rodionnaboka09@gmail.com>
2024-12-06 18:51:44 +0200

    hi

---
+++ main.py
@@ -0,0 +1,16 @@
+from sonfig
+
+"""
+
+git init
+
+git status
+git log
+git config --global user.name 'Rodion Naboka'
+git config --global user.email '<rodionnaboka09@gmail.com>>
+
+git add <path_to_file>
+git commit -m ''
+
+git log
+"""
